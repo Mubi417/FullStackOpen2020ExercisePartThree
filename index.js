a531416382ab97d3d4fb79a013852ab752a0d23c
mubi417 <mubi417@gmail.com>
2020-08-03 15:52:23 +0100

    correct ports for heroku deployment

--- index.js
+++ index.js
@@ -90,5 +90,7 @@ app.get('/info', (req, res) => {
 })
 
 
-app.listen(3001)
-console.log("server started at 3001")
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
